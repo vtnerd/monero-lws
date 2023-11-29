a23eadd7ef0638081fec32c76d87a6bc1d29ecdd
Lee *!* Clagett <vtnerd@users.noreply.github.com>
2023-11-29 16:00:32 -0500

    Fix output_id bug with coinbase transactions (#88)

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -407,7 +407,7 @@ namespace lws
             db::output{
               db::transaction_link{height, tx_hash},
               db::output::spend_meta_{
-                db::output_id{out.amount, out_ids.at(index)},
+                db::output_id{tx.version < 2 ? out.amount : 0, out_ids.at(index)},
                 amount,
                 mixin,
                 boost::numeric_cast<std::uint32_t>(index),
